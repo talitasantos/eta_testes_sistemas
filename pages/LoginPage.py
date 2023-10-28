ff245c5d5c1c84d38592099d92b23e6073a83305
talitasantos2310@gmail.com <Maycon@1802>
2023-10-28 13:28:05 -0300

    Parte da manhã: 28/10

--- pages/LoginPage.py
+++ pages/LoginPage.py
@@ -1,13 +1,15 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
+from pages.PageObject import PageObject
 
-class LoginPage:
+
+class LoginPage(PageObject):
 
     url = 'https://www.saucedemo.com/'
 
-    def __init__(self):
-        self.driver = webdriver.Chrome()
+    def __init__(self, browser):
+        super(LoginPage, self).__init__(browser=browser)
 
     def open_page(self):
         self.driver.get(self.url)
@@ -15,18 +17,17 @@ class LoginPage:
     def click_login_btn(self):
         self.driver.find_element(By.ID, 'login-button').click()
 
-    def is_url(self):
-        return self.driver.current_url == self.url
+    def is_url_login(self):
+        return self.is_url(self.url)
 
     def has_login_error_message(self):
         error_message = self.driver.find_element(By.CLASS_NAME, 'error-message-container').text
         return error_message == 'Epic sadface: Username is required'
 
+    def is_page(self, url, title_text):
+        raise Exception('Essa página nao tem titutlo')
+
     def enter_login(self, user_name='standard_user', password='secret_sauce'):
         self.driver.find_element(By.ID, 'user-name').send_keys(user_name)
         self.driver.find_element(By.CSS_SELECTOR, '[data-test="password"]').send_keys(password)
         self.click_login_btn()
-
-    def close(self):
-        self.driver.quit()
-
--- tests/conftest.py
+++ tests/conftest.py
@@ -3,9 +3,22 @@ import pytest
 from pages.LoginPage import LoginPage
 
 
-@pytest.fixture
-def open_browser():
-    login_p = LoginPage()
+def pytest_addoption(parser):
+    parser.addoption("--browser_selenium", default='chrome', help='Select browser')
+
+
+@pytest.fixture()
+def open_browser(request):
+    select_browser = request.config.getoption("--browser_selenium").lower()
+    login_p = LoginPage(browser=select_browser)
     login_p.open_page()
     yield login_p
     login_p.close()
+
+
+@pytest.fixture()
+def login_saucedemo(open_browser):
+    login_p = open_browser
+    login_p.enter_login()
+    yield login_p
+
