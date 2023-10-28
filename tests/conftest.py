4c4e63c9ff259277f690d3240392ad3385703efc
talitasantos2310@gmail.com <Maycon@1802>
2023-10-28 14:22:06 -0300

    Rodando em mais de um browser.

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,17 +4,25 @@ from pages.LoginPage import LoginPage
 
 
 def pytest_addoption(parser):
-    parser.addoption("--browser_selenium", default='chrome', help='Select browser')
+    parser.addoption("--browser", default='chrome', help='Select browser')
 
 
 @pytest.fixture()
 def open_browser(request):
-    select_browser = request.config.getoption("--browser_selenium").lower()
+    select_browser = request.config.getoption("--browser").lower()
+    if select_browser not in ['chrome', 'safari', 'firefox']:
+        raise Exception('Browser not found!')
     login_p = LoginPage(browser=select_browser)
     login_p.open_page()
     yield login_p
     login_p.close()
 
+@pytest.fixture()
+def run_all_browser(all_browsers):
+    login_p = LoginPage(browser=all_browsers)
+    login_p.open_page()
+    yield login_p
+    login_p.close()
 
 @pytest.fixture()
 def login_saucedemo(open_browser):
